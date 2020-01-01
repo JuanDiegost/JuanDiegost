97c040fc482ac3257ed92df0612f80801fa9db57
JuanDiegost <molinajuan149@gmail.com>
2019-12-31 20:02:21 -0500

    Agregar skills

---
+++ precache-manifest.4c89b8436d9ab7d9f85be3adc2d0f621.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc435bb96b66c9773c544a5e6e8732e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a571323732e497641f",
+    "url": "/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "be72820f946385450d79",
+    "url": "/static/css/main.1b7fe9c2.chunk.css"
+  },
+  {
+    "revision": "76a571323732e497641f",
+    "url": "/static/js/2.f2fde32a.chunk.js"
+  },
+  {
+    "revision": "be72820f946385450d79",
+    "url": "/static/js/main.cfb9f7df.chunk.js"
+  },
+  {
+    "revision": "1baad8a9a37b534391b5",
+    "url": "/static/js/runtime-main.50791c72.js"
+  },
+  {
+    "revision": "8c8e396e76e25d0f86dbb10988a2d33d",
+    "url": "/static/media/developer.8c8e396e.webp"
+  },
+  {
+    "revision": "29aef0fdcb6ca83512b03ea3ab7eb755",
+    "url": "/static/media/programmer.29aef0fd.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8944d170e64ac3c8969890d5cbc0dab0.js"
+  "/precache-manifest.4c89b8436d9ab7d9f85be3adc2d0f621.js"
 );
 
 self.addEventListener('message', (event) => {
