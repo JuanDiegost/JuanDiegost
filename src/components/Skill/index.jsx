e2da6db2e25d070105fd4cad02fa5d49a3a45127
Juan Diego Molina M <juan.munoz@novatec.com.co>
2022-08-07 11:34:05 -0500

    style: Se agregan estilos para responsive skills y darkmode 🎨

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -6,7 +6,7 @@ function createSVG(path) {
 
 export default function Skill(props) {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center">
+    <div className="flex flex-col gap-2 sm:justify-center sm:items-center">
         <svg
           className="hidden sm:block"
           xmlns="http://www.w3.org/2000/svg"
@@ -14,9 +14,9 @@ export default function Skill(props) {
           fill="currentColor"
           dangerouslySetInnerHTML={createSVG(props.img)}
         ></svg>
-      <div className="flex flex-col items-center justify-center">
-        <span className="text-center">{props.name}</span>
-        <span className="font-light text-gray-500 dark:text-gray-400 text-center">
+      <div className="flex flex-row gap-2 md:flex-col items-start justify-start sm:items-center sm:justify-center">
+        <span className="text-left sm:text-center">{props.name}</span>
+        <span className="text-left font-light text-gray-500 dark:text-gray-400 sm:text-center">
           {props.time}
         </span>
       </div>
