f0eb3f294c90f93797927c14ad54885ddfca42b9
Juan Diego Molina M <juan.munoz@novatec.com.co>
2022-08-06 21:55:16 -0500

    fix: Se elimina la configuracion ssr no necesaria :bug:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  adapter: vercel()
+  integrations: [tailwind(), react()]
 });
--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,14 +1,14 @@
 export default function Work(props) {
   return (
-    <li class="mb-10 ml-4">
-      <div class="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+    <li className="mb-10 ml-4">
+      <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
         {props.start} - {props.end}
       </time>
-      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
         {props.company}
       </h3>
-      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+      <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         {props.description}
       </p>
     </li>
